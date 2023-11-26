d736d8575e04e755674833060841b89462c93735
arianh00 <arianh00@estudiantes.unileon.es>
2023-11-26 14:50:39 +0100

    ADD new functions to better space code

--- menu.sh
+++ menu.sh
@@ -1,31 +1,71 @@
 #!/bin/bash
+leer_codigo ()
+{
+    echo 'Escriba el archivo que desea visualizar (El archivo tiene que estar contenido en este directorio)'
+    echo
+    read archivo
+    if [[ -f $archivo ]]; then
+        cat $archivo
+        echo
+    else
+        echo 'Dicho archivo no existe o no se puede visualizar'
+    fi
+}
+
+compilar_codigo ()
+{
+    echo 'Elija el archivo que desea compilar. Este archivo debe de tener una terminación .c'
+    echo 'El archivo se compilara como: (simulador)'
+    echo
+    read archivo
+    re="^[^[:space:]]+\.c$"
+    if [[ -f $archivo && $archivo =~ $re ]]; then
+        gcc $archivo -o simulador
+        echo
+    else
+        echo 'Dicho archivo no existe o no se puede compilar'
+    fi
+}
+
 menu ()
 {
-    while [[ true ]]; do
-        printf "Elija una opción:\n1)Leer código del programa\n2)Compilar programa\n3)Ejecutar programa\n4)Salir\n\n"
-        read seleccion
-        echo
-        case $seleccion in
-            1) cat simulador.c
-                echo; echo
-            ;;
-            2) gcc simulador.c -o simulador
-            ;;
-            3) if [[ -f "simulador" && -x 'simulador' ]]; then
-                echo "TODO"; echo
-            else
-                printf "No se ha encontrado el archivo o bien este no se puede ejecutar\n"
-                echo
-            fi
-            ;;
-            4) echo "Hasta luego"
-                break 
-            ;;
-            *) echo "La opción no es valida, pruebe otra vez"
-            ;;
-        esac
-    done
-        
+    case $1 in
+        1) leer_codigo
+            echo
+        ;;
+        2) compilar_codigo
+            echo
+        ;;
+        3) if [[ -f "simulador" && -x 'simulador' ]]; then
+            echo "TODO"; echo
+        else
+            printf "No se ha encontrado el archivo o bien este no se puede ejecutar\n"
+            echo
+        fi
+        ;;
+        4) echo "Hasta luego"
+        ;;
+        *) echo "La opción no es valida, pruebe otra vez"
+            echo
+        ;;
+    esac        
 }
-    
-menu
+
+print_menu ()
+{
+    echo 'Elija una opción:'
+    echo '1)Leer código del programa'
+    echo '2)Compilar el programa' 
+    echo '3)Ejecutar el programa'
+    echo '4)Salir'
+    echo 
+}
+
+seleccion=0    
+while [[ $seleccion -ne 4 ]]; do
+    print_menu
+    read seleccion
+    echo
+    menu $seleccion
+done
+
