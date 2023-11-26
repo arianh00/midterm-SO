164962562ff91f59fa4aaa58a9875f57f682b305
arianh00 <arianh00@estudiantes.unileon.es>
2023-11-26 15:23:24 +0100

    ADD comments

--- simulador.c
+++ simulador.c
@@ -12,14 +12,27 @@ void handler_asistentes();
 int main (int argc, char *argv[]) {
     pid_t tecnico;
     pid_t encargado;
+    // Compruebo que solo haya un argumento
     if(argc != 2){
         printf("No se han recibido unos argumentos adecuados\n");
         exit(-1);
     }
 
+    // Convierto el número recibido por argumentos en un entero
     int numAsistentes = atoi(argv[1]);
+
+    // Compruebo que asistentes no sea 0
+    // Esto se puede dar o bien porque ha recibido un cero por linea de comandos
+    // O bien porque atoi no ha podido convertir el string(por default devuelve 0)
+    if (numAsistentes == 0)
+    {
+        printf("No se pudo establecer correctamente el número de asistentes\n");
+        exit(-1);
+    }
+    
     pid_t* asistentes = (pid_t*)malloc(sizeof(pid_t)*numAsistentes);
 
+    // Creo todos los procesos en un for
     for (size_t i = 0; i < numAsistentes+2; i++)
     {
         pid_t pid;
@@ -31,6 +44,7 @@ int main (int argc, char *argv[]) {
         {
             switch (i)
             {
+                // Asigno a cada proceso su tratamiento de señal
                 case 0:
                     printf("Tecnico creado\n");
                     if(signal(SIGUSR1, handler_tecnico) == SIG_ERR){
@@ -45,6 +59,8 @@ int main (int argc, char *argv[]) {
                         exit(-1);
                     }
                     break;
+                // Si estamos en una iteración diferente a la 0 o la 1 entonces ya estamos creando asistentes
+                // De ahí que se use el default para el switch
                 default:
                     printf("Asistente %d creado\n", (int)(i-1));
                     if(signal(SIGUSR2, handler_asistentes) == SIG_ERR){
@@ -55,6 +71,7 @@ int main (int argc, char *argv[]) {
             }
             pause();
         }else{
+            // Guardo los pids para posteriormente poder hacer las llamada a kills
             switch (i)
             {
                 case 0:
@@ -74,12 +91,16 @@ int main (int argc, char *argv[]) {
     
     sleep(2);
     
+    // Se manda la señal SIGUSR1 al tecnico hasta que la llamada no falle
     while(kill(tecnico, SIGUSR1) != 0){
         perror("Lanzamiento de señal SIGUSR1 al tecnico ha fallado.");
     }
     
+    // Esperamos a que el técnico termine
     waitpid(tecnico, &estado_tecnico, 0);
     
+    // Si el vuelo no es viable (exit con 0), entonces matamos los procesos y salimos
+    // Se sale con 0 dado que no ha ocurrido ningún error
     if(WEXITSTATUS(estado_tecnico) == 0){
         printf("El vuelo no es viable\n");
         kill(encargado, SIGKILL);
@@ -90,16 +111,19 @@ int main (int argc, char *argv[]) {
         exit(0);
     }
     
+    // Se manda la señal SIGUSR1 al encargado hasta que la llamada no falle
     while(kill(encargado, SIGUSR1) != 0){
         perror("Lanzamiento de señal SIGUSR1 al encargado ha fallado.");
     }
     
+    // Esperamos a que el encargado termine
     waitpid(encargado, &estado_encargado, 0);
     
     int pasajeros = 0;
     
     for (size_t i = 0; i < numAsistentes; i++)
     {
+        // Mandamos la señal SIGUSR2 a todos los asistentes hasta que ninguna falle
         while(kill(asistentes[i], SIGUSR2) != 0){
             perror("Lanzamiento de señal SIGUSR2 a un asistente ha fallado.");
         }
@@ -108,10 +132,12 @@ int main (int argc, char *argv[]) {
     int pasajeros_embarcados = 0;
     for (size_t i = 0; i < numAsistentes; i++)
     {
+        // Esperamos a que cualquiera de los asistentes termine y añadimos los pasajeros al total
         wait(&pasajeros_embarcados);
         pasajeros += WEXITSTATUS(pasajeros_embarcados);
     }
     
+    // Si había overbooking entonces restamos 10 al total de pasajeros
     if(WEXITSTATUS(estado_encargado) == 1){
         pasajeros -= 10;
     }
@@ -119,6 +145,8 @@ int main (int argc, char *argv[]) {
     free(asistentes);
 }
 
+// A todos los handler se les ha dado su propia semilla de random para que no se generen repeticiiones
+// Esto se ha hecho estableciendo los pids como semillas
 void handler_tecnico(){
     if(signal(SIGUSR1, handler_tecnico) == SIG_ERR){
         perror("Llamada a signal fallida.");
@@ -126,9 +154,11 @@ void handler_tecnico(){
     }
     srand(getpid());
     printf("Se ha recibido la señal, comprobando si el vuelo es viable.\n");
+    // Se duerme un tiempo aleatorio entre 3 y 6
     int tiempo_sueno = (rand() % 4) + 3;
     sleep(tiempo_sueno);
     int viabilidad;
+    // Si el vuelo es viable lo especifica, y sino simplemente sale con 0
     if((viabilidad = (rand() % 2)) == 0){
         exit(viabilidad);
     }
@@ -145,6 +175,7 @@ void handler_encargado(){
     printf("Se ha recibido la señal, comprobando si hay overbooking.\n");
     int overbooking = rand() % 2;
     sleep(2);
+    // Imprime un mensaje dependiendo de si hay overbooking o no
     if(overbooking == 0){
         printf("No hay overbooking.\n");
         exit(overbooking);
@@ -160,9 +191,11 @@ void handler_asistentes(){
     }
     srand(getpid());
     printf("Se ha recibido la señal, procediendo con un embarque.\n");
+    // Se duerme un numero aleatorio entre 3 y 6
     int tiempo_embarque = (rand() % 4) + 3;
     sleep(tiempo_embarque);
     printf("El embarque ha durado %ds.\n", tiempo_embarque);
+    // Embarca entre 20 y 30 personas
     int embarcados = (rand() % 11) + 20;
     printf("Se han embarcado %d personas.\n", embarcados);
     exit(embarcados);
